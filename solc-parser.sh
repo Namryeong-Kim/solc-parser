d9b7a05aa63f82880a530d9b7f5eba8164e08681
Namryeong-Kim <namryeong.kim@dream-academy.io>
2023-05-21 18:02:41 +0900

    success > and < case

--- solc-parser.sh
+++ solc-parser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 solidity_file="$1"  # Solidity 파일 경로
-# python_script="version_parser.py"  # Python 스크립트 파일명
+# python_script="get_version_list.py"  # Python 스크립트 파일명
 
 # # Solidity 버전 리스트 생성
 # version_create=$(python3 "$python_script")
@@ -26,8 +26,12 @@ if [[ -n "$target_version" && ( -z "$target_sign" || "$target_sign" == "=" || "$
 
 elif [[ -n "$target_version" && ( "$target_sign" == "^" || "$target_sign" == "~" || "$target_sign" == ">=" ) ]]; then
     print_version_info
-    highest_version=$(python3 search_highest_version.py "$target_version")
-    install_solc "$highest_version"
+    get_version=$(python3 search_highest_version.py "$target_version")
+    install_solc "$get_version"
+elif [[ -n "$target_version" && ( "$target_sign" == "<" || "$target_sign" == ">" ) ]]; then
+    print_version_info
+    get_version=$(python3 search_less_or_greater_case.py "$target_version" "$target_sign")
+    install_solc "$get_version"
 else
   echo "Solidity version not found."
 fi
